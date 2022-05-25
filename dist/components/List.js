beffc9c6a940954b414646880544f052d892e325
Sergio H <sergiohernandezroman@gmail.com>
2022-05-25 23:22:10 +0200

    add Try to "click" delete

--- dist/components/List.js
+++ dist/components/List.js
@@ -8,11 +8,11 @@ export class List extends Component {
         this.series = sseries;
         this.template = this.createTemplate();
         this.outRender(selector);
+        this.manageComponent();
         this.series.forEach((item) => {
             if (item.watched === false) {
                 new Serie(item, 'slot.pending');
-            }
-            else {
+            } else {
                 new Serie(item, 'slot.watched');
             }
         });
@@ -23,19 +23,32 @@ export class List extends Component {
           <h2 class="section-title">Series list</h2>
           <section class="series-pending">
             <h3 class="subsection-title">Pending series</h3>
-            <p class="info">You have 4 series pending to watch</p>
+            <p class="info">You have 2 series pending to watch</p>
             <ul class="series-list">
              <slot class="pending"></slot> 
             </ul>
           </section>
           <section class="series-watched">
             <h3 class="subsection-title">Watched series</h3>
-            <p class="info">You have watched 4 series</p>
+            <p class="info">You have watched 3 series</p>
             <ul class="series-list series-list--watched">
               <slot class="watched"></slot>
             </ul>
           </section> 
         </section>
     `;
+    }
+    manageComponent() {
+        document
+            .querySelectorAll('.fas fa-times-circle icon--delete')
+            .forEach((item) =>
+                item.addEventListener('click', this.handlerButton.bind(this))
+            );
+    }
+    handlerButton(ev) {
+        const deletedId = ev.target.dataset.id;
+        console.log('click', deletedId);
+        this.series = this.series.filter((item) => item.id !== deletedId);
+        this.template = this.createTemplate();
     }
 }
--- src/components/List.ts
+++ src/components/List.ts
@@ -1,5 +1,5 @@
 import { iSerie } from './interface';
-import { series as sseries } from '../scripts/series.js';
+import { series } from '../scripts/series.js';
 import { Component } from './component.js';
 import { Serie } from './serie.js';
 
@@ -7,9 +7,10 @@ export class List extends Component {
     series: Array<iSerie>;
     constructor(selector: string) {
         super();
-        this.series = sseries;
+        this.series = series;
         this.template = this.createTemplate();
         this.outRender(selector);
+        this.manageComponent();
         this.series.forEach((item) => {
             if (item.watched === false) {
                 new Serie(item, 'slot.pending');
@@ -24,19 +25,30 @@ export class List extends Component {
           <h2 class="section-title">Series list</h2>
           <section class="series-pending">
             <h3 class="subsection-title">Pending series</h3>
-            <p class="info">You have 4 series pending to watch</p>
+            <p class="info">You have 2 series pending to watch</p>
             <ul class="series-list">
              <slot class="pending"></slot> 
             </ul>
           </section>
           <section class="series-watched">
             <h3 class="subsection-title">Watched series</h3>
-            <p class="info">You have watched 4 series</p>
+            <p class="info">You have watched 3 series</p>
             <ul class="series-list series-list--watched">
               <slot class="watched"></slot>
             </ul>
           </section> 
         </section>
     `;
+    }
+    private manageComponent() {
+        document
+            .querySelectorAll('.fas fa-times-circle icon--delete')
+            .forEach((item) =>
+                item.addEventListener('click', this.handlerButton.bind(this))
+            );
+    }
+    private handlerButton(ev: Event) {
+        const deletedId = (<HTMLElement>ev.target).dataset.id;
+        console.log('click', deletedId);
     }
 }
